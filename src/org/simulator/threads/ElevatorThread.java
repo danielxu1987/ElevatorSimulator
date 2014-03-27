f271b3e7194bd3d16d8b0fb9f42d0d215914d2a9
danielxu1987 <xusifeng@gmail.com>
2014-03-27 14:00:33 +0100

    Add comments to Elevator class
    Comments in class Elevator is added for a further implementation of
    multithreading between CallButton and ELevator.

--- src/org/simulator/threads/ElevatorThread.java
+++ src/org/simulator/threads/ElevatorThread.java
@@ -3,17 +3,19 @@ package org.simulator.threads;
 import org.simulator.mainLogic.Building;
 
 // study the sample code from sync producer/consumer
-// person on floor 0 and person on floor 1 can be producer and consumer
+// callbuttons at floor0 and floor1 can be producer and consumer
 // and this class is comparable to HoldIntegerSynchronised
-// and parameter 'movable' can be patterned after parameter 'writable' 
+// and parameter 'movable' can be a shared memory cell constantly sync
+// by callbutton0 and callbutton1. So if callbutton0 wants to call elevator
+// which is not free then it should wait until elevator is free. The same 
+// with callbutton1
 /** should contain synchronised methods to monitor the call button*/
-public class ElevatorThread
+public class ElevatorThread 
 {
 	private boolean movable;
 	
 	public ElevatorThread(Building targetBuilding)
 	{
-		
 	}
 
 	public synchronized void callElevator()
